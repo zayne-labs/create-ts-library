89610607b5e5c9734e61af8322c847e5304b6a08
ryan-zayne <phoenixastro97@gmail.com>
2024-10-18 01:16:21 +0100

    chore: update pkg.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-import zayne from "@zayne-labs/eslint-config";
+import { zayne } from "@zayne-labs/eslint-config";
 
 export default zayne({
-	ignores: ["dist/**", "eslint.config.js"],
+	ignores: ["dist/**"],
 	typescript: {
 		tsconfigPath: "tsconfig.eslint.json",
 	},
