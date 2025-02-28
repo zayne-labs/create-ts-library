075ccc6198ce0f8f240ab7ce7287e582b1cc21a5
Ryan-Zayne <phoenixastro97@gmail.com>
2025-02-28 17:51:37 +0100

    update

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+/**
+ * @import { Config } from "prettier"
+ * @type { Config }
+ */
+
+const config = {
+	experimentalOperatorPosition: "start",
+	printWidth: 107,
+	singleQuote: false,
+	tabWidth: 3,
+	trailingComma: "es5",
+	useTabs: true,
+};
+
+export default config;
